6e1185e4e365df7bda31b3d80b402538cd9d4a7b
Diego Molina <dmolina@resimple.cl>
2025-04-15 23:50:14 -0400

    Commit inicial para comenzar proyecto desde una base

---
+++ src/pages/public/HomePage.jsx
@@ -0,0 +1,19 @@
+import HeroSection from '../../sections/HeroSection';
+import AboutSection from '../../sections/AboutSection';
+import ProjectsSection from '../../sections/ProjectsSection';
+import ContactSection from '../../sections/ContactSection';
+import SkillsSection from '../../sections/SkillsSection';
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <SkillsSection /> 
+      <ProjectsSection />
+      <ContactSection />
+    </>
+  );
+};
+
+export default HomePage;
